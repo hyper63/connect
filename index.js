25a87c27873d743301dac55ae02584bd40072f12
Tom Wilson <twilson63@gmail.com>
2021-05-01 11:21:23 +0000

    added some jsdoc

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ if (!process.env.HYPER) { throw new Error('HYPER connection string is not set') 
 
 const u = new URL(process.env.HYPER)
 
-module.exports = {
-  url: (svc, ...paths) => 
-    [{svc, paths}]
+/**
+ * @param {string} svc - hyper service
+ * @param {...string} paths - zero to many additional path names
+ * @retuns {string}
+ */
+function url(svc, ...paths) {
+  // 0 dependency identity functor
+  return [{svc, paths}]
       .map(({svc, paths}) => ({svc, pathname: [u.pathname, ...paths].join('/')}))
       .map(({svc, pathname}) => `https://${u.host}/${svc}${pathname}`)
       .shift()
-  ,
-	token: () => jwt.sign({ sub: u.username }, u.password)
 }
+
+/**
+ * @param {object} [payload]
+ * @returns {string}
+ */
+function token(payload) {
+  return jwt.sign({ sub: u.username, ...payload}, u.password)
+}
+
+module.exports = { url, token }
